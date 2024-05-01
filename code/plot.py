0990d3a7ba050d7a46a895b0de96a15219c4aeda
Dennis Mulligan <dmulliga@ucsc.edu>
2024-05-01 11:10:18 -0700

    typo

--- code/plot.py
+++ code/plot.py
@@ -195,7 +195,7 @@ class PS_distribution:
         self.legend.set_xticks([])
         self.legend.set_yticks([])  
         self.legend.set_xlim(0,x+1)
-        if len(self.labels > 5):
+        if len(self.labels) > 5:
             self.legend.set_ylim(0,6)
         else:
             self.legend.set_ylim(y,6)         
